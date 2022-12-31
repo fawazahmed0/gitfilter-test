2f89f964de18d7e3fc0fd771a91af47112a112f1
Fawaz Ahmed <fawazahmed0@hotmail.com>
2022-12-31 22:02:33 +0530

    Revert "Update rebase-editor.js"
    This reverts commit 22b17849ef295fc55034c256ffb27257cd4cc5c6.

--- rebase-editor.js
+++ rebase-editor.js
@@ -11,17 +11,5 @@ let arr = data.trim().split(/\r?\n/).filter(e=>e.trim().startsWith('pick'))
 arr.unshift(arr[arr.length-1])
 arr[arr.length-1] = ''
 
-// squash n commits at a time
-let batch = 30
-let count = 0
-
-for(let i=1;i<arr.length-1;i++){
-
-    if(/update currencies/i.test(arr[i]) && /update currencies/i.test(arr[i-1]) && ++count%batch!=0)
-     arr[i] = arr[i].replace('pick', 'squash')
-
-}
-
-
 fs.writeFileSync(process.argv[2],arr.join('\n'))
 //fs.writeFileSync(path.join(__dirname, 'temp2.txt'),arr.join('\n'))
